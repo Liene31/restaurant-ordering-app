f47880e0d4ebfb711313a24ece8fe8986d6041fe
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-05-15 12:38:58 +0200

    Added handlePaymentBtn() & renderOrderConfirmationMsg()

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { menuArray } from "/data.js";
 
 let orderedItems = [];
+const innerModal = document.getElementById("inner-modal");
+const orderSection = document.getElementById("order-section");
+const orderConfirmation = document.getElementById("order-confirmation");
 
 document.getElementById("page-wrapper").addEventListener("click", handleClicks);
 
@@ -11,12 +14,16 @@ function handleClicks(e) {
     handleRemoveBtn(Number(e.target.dataset.index));
   } else if (e.target.id === "order-complete-btn") {
     handleOrderCompleteBtn();
+  } else if (e.target.id === "payment-form-btn") {
+    e.preventDefault();
+    handlePaymentBtn();
   }
 }
 
 // Handles the item add button
 
 function handleMenuAddBtn(id) {
+  orderConfirmation.innerHTML = "";
   const menuItem = menuArray.filter((item) => {
     return item.id === id;
   })[0];
@@ -35,15 +42,33 @@ function handleRemoveBtn(index) {
 }
 
 function handleOrderCompleteBtn() {
-  const innerModal = document.getElementById("inner-modal");
   innerModal.style.display = "block";
+}
+
+function handlePaymentBtn() {
+  const nameInput = document.getElementById("name-input");
+
+  innerModal.style.display = "none";
+  renderOrderConfirmationMsg(nameInput.value);
+  nameInput.value = "";
+  document.getElementById("input-card-number").value = "";
+  document.getElementById("input-cvv-number").value = "";
+  orderSection.style.display = "none";
+}
+
+function renderOrderConfirmationMsg(name) {
+  orderConfirmation.innerHTML = `
+    <div class="order-confirmation-inner" id="order-confirmation-inner">
+      <p>Thanks, ${name}! Your order is on its way!</p>
+    </div>
+  `;
+  orderedItems = [];
 }
 
 // Render section of ordered items
 
 function renderOrderedItems() {
   const orderItemInner = document.getElementById("order-item-inner");
-  const orderSection = document.getElementById("order-section");
 
   if (orderedItems.length) {
     orderItemInner.innerHTML = orderedItems
